ebfcdc46903cb637cf74dd10845cca90e83db553
Aaron Schnarr <aaron.schnarr@quartus.com>
2021-08-24 16:01:41 -0700

    Added Redis Repository

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+	public class ShoppingCart
+	{
+		public ShoppingCart()
+		{
+
+		}
+		public ShoppingCart(string userName)
+		{
+			UserName = userName;
+		}
+
+		public string UserName { get; set; }
+		public List<ShoppingCartItem> Items { get; set; } = new List<ShoppingCartItem>();
+
+		public decimal TotalPrice
+		{
+			get
+			{
+				var totalPrice = 0m;
+				foreach (var item in Items)
+				{
+					totalPrice += item.Price * item.Quantity;
+				}
+
+				return totalPrice;
+			}
+		}
+	}
+}
